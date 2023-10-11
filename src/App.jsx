d315ea07922f7face3565e2adf68e98b8db5b3fe
GoJiMin <gojimin3095@gmail.com>
2023-10-11 10:45:53 +0900

    style: Global CSS 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import Navbar from "../Navbar";
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <Navbar />
+        <Outlet />
+      </header>
+    </>
+  );
 }
 
 export default App;
