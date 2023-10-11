9d053e3babed65d48d83a2ed1d48cc3d0b3de59c
GoJiMin <gojimin3095@gmail.com>
2023-10-11 11:51:58 +0900

    feat: Home 화면 구성

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import styles from "./Home.module.css";
+import Button from "../components/Button";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <section className={styles.section}>
+      <h1>오늘 뭐 읽지?</h1>
+      <div>
+        <p>태그를 선택해 오늘 읽을 책을 추천 받아보세요.</p>
+      </div>
+      <Link to='/select'>
+        <Button text={"Get Started"} />
+      </Link>
+    </section>
+  );
 }
