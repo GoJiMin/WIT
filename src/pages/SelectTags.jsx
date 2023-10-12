932150623ecb86a2e33beb6f30be4bbfb77e6cc0
GoJiMin <gojimin3095@gmail.com>
2023-10-13 00:25:02 +0900

    feat: SelectTags.jsx UI 구성 및 tagData의 상수 데이터를 이용한 필터 작업

--- src/pages/SelectTags.jsx
+++ src/pages/SelectTags.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
+import { DETAIL_LIST, TAG_LIST } from "../data/tagData";
+import Button from "../components/Button";
+import styles from "./SelectTags.module.css";
 
 export default function SelectTags() {
-  return <div>SelectTags</div>;
+  const [tag, setTag] = useState();
+  const handleClick = (e) => {
+    setTag(e.target.id);
+  };
+  return (
+    <>
+      <ul className={styles.tagList}>
+        {TAG_LIST.map(({ id, tagName }) => (
+          <li className={styles.tag} key={id} onClick={handleClick}>
+            <Button text={tagName} id={id} />
+          </li>
+        ))}
+      </ul>
+      {tag && (
+        <ul className={styles.detailList}>
+          {DETAIL_LIST[tag].map(({ id, tagName }) => (
+            <li className={styles.tag} key={id}>
+              <Button text={tagName} id={id} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
