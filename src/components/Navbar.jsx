4c90c1b39d11dc46544bfca5eec6a51917e7bc5b
GoJiMin <gojimin3095@gmail.com>
2023-10-31 22:57:56 +0900

    feat: login 기능 구현 - firebase

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
-import { PiBookOpenBold } from "react-icons/pi";
+import { login, logout, onUserStateChange } from "../services/firebase";
 
 export default function Navbar() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    onUserStateChange((user) => {
+      setUser(user);
+    });
+  }, []);
+
+  const handleLogin = () => {
+    login().then(setUser);
+  };
+
+  const handleLogout = () => {
+    logout().then(setUser);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.titleBox}>
@@ -16,7 +32,16 @@ export default function Navbar() {
       <div className={styles.list}>
         <p className={styles.element}>건의하기</p>
         <p className={styles.element}>서재</p>
-        <p className={styles.element}>로그인</p>
+        {!user && (
+          <button className={styles.element} onClick={handleLogin}>
+            로그인
+          </button>
+        )}
+        {user && (
+          <button className={styles.element} onClick={handleLogout}>
+            로그아웃
+          </button>
+        )}
       </div>
     </header>
   );
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,4 +1,11 @@
 import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -8,3 +15,35 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+
+export async function login() {
+  return signInWithPopup(auth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+
+      return user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.customData.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+
+      console.log(error);
+    });
+}
+
+export async function logout() {
+  signOut(auth).then(() => null);
+}
+
+export function onUserStateChange(callback) {
+  onAuthStateChanged(auth, (user) => {
+    callback(user);
+  });
+}
