6ed0df0a5e609701070a94ff30f1734f160743e0
GoJiMin <gojimin3095@gmail.com>
2023-10-31 22:32:41 +0900

    feat: firebase config env setting

---
+++ src/services/firebase.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
