277fc8ad82cefb392fbfe26410f627d94cc8bbe9
GoJiMin <gojimin3095@gmail.com>
2023-11-02 23:26:50 +0900

    feat: 파이어베이스 데이터베이스 접근 함수 구현

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,12 +1,12 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import {
   getAuth,
   signInWithPopup,
   GoogleAuthProvider,
   signOut,
   onAuthStateChanged,
-} from 'firebase/auth';
-import { getDatabase } from 'firebase/database';
+} from "firebase/auth";
+import { getDatabase, ref, set, get, remove } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -19,7 +19,7 @@ const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 
 export function login() {
   signInWithPopup(auth, provider).catch(console.error);
@@ -33,4 +33,20 @@ export function onUserStateChange(callback) {
   onAuthStateChanged(auth, (user) => {
     callback(user);
   });
+}
+
+export async function getLibrary(userId) {
+  return get(ref(database, `libraries/${userId}`)) //
+    .then((snapshot) => {
+      const books = snapshot.val() || {};
+      return Object.values(books);
+    });
+}
+
+export async function addUpdateToLibrary(userId, book) {
+  return set(ref(database, `libraries/${userId}/${book.isbn13}`), book);
+}
+
+export async function removeFromLibrary(userId, isbn13) {
+  return remove(ref(database, `libraries/${userId}/${isbn13}`));
 }
