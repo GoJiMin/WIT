09cd7045c021a7fe51ef64687b80b61e7ee107a0
GoJiMin <gojimin3095@gmail.com>
2023-11-06 16:42:09 +0900

    feat: SearchBooks 페이지의 api 병렬 처리를 위한 useQueries 사용

--- src/pages/SearchBooks.jsx
+++ src/pages/SearchBooks.jsx
@@ -1,14 +1,13 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { searchToTag } from "../services/aladin";
 import Book from "../components/Book.jsx";
 import styles from "./SearchBooks.module.css";
 import Button from "../components/Button.jsx";
 import { HiArrowUp } from "react-icons/hi2";
-import { useQuery } from "@tanstack/react-query";
+import { useQueries } from "@tanstack/react-query";
 import { useAuthContext } from "../context/AuthContext";
 import { getBookMarks } from "../services/firebase";
-import { Suspense } from "react";
 
 export default function SearchBooks() {
   const { uid } = useAuthContext();
@@ -16,15 +15,18 @@ export default function SearchBooks() {
   const { categoryId } = useParams();
   const navigate = useNavigate();
 
-  const {
-    isLoading,
-    isFetching,
-    data: books,
-    refetch,
-  } = useQuery(["bookData", categoryId], () => searchToTag(categoryId), {
-    suspense: true,
+  const [{ data: books, refetch }, { data: bookMarks }] = useQueries({
+    queries: [
+      {
+        queryKey: ["bookData", categoryId],
+        queryFn: () => searchToTag(categoryId),
+      },
+      {
+        queryKey: ["bookMarks"],
+        queryFn: () => getBookMarks(uid),
+      },
+    ],
   });
-  const { data: bookMarks } = useQuery(["bookMarks"], () => getBookMarks(uid));
 
   const handleSearch = () => {
     refetch();
@@ -39,45 +41,38 @@ export default function SearchBooks() {
     navigate(-1);
   };
 
-  console.log(isLoading);
-
   // if (isLoading) return <p>Loading...</p>;
   // if (isFetching) return <p>Loading...</p>;
 
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <>
-        {books && (
-          <section className={styles.section}>
-            <ul className={styles.bookList} ref={listBox}>
-              {books &&
-                books.item.map((book) => (
-                  <li key={book.isbn13}>
-                    <Book
-                      data={book}
-                      type={"add"}
-                      animation={true}
-                      hasBookMark={bookMarks?.includes(book.isbn13)}
-                    />
-                  </li>
-                ))}
-            </ul>
-            <div className={styles.search}>
-              <Button
-                text={"다시 선택할래요. 🔍"}
-                handleFunction={handleClick}
-              />
-              <Button
-                text={"다시 추천 받을래요. 🤔"}
-                handleFunction={handleSearch}
-              />
-            </div>
-            <button className={styles.up} onClick={scrollToTop}>
-              <HiArrowUp />
-            </button>
-          </section>
-        )}
-      </>
-    </Suspense>
+    <>
+      {books && (
+        <section className={styles.section}>
+          <ul className={styles.bookList} ref={listBox}>
+            {books &&
+              books.item.map((book) => (
+                <li key={book.isbn13}>
+                  <Book
+                    data={book}
+                    type={"add"}
+                    animation={true}
+                    hasBookMark={bookMarks?.includes(book.isbn13)}
+                  />
+                </li>
+              ))}
+          </ul>
+          <div className={styles.search}>
+            <Button text={"다시 선택할래요. 🔍"} handleFunction={handleClick} />
+            <Button
+              text={"다시 추천 받을래요. 🤔"}
+              handleFunction={handleSearch}
+            />
+          </div>
+          <button className={styles.up} onClick={scrollToTop}>
+            <HiArrowUp />
+          </button>
+        </section>
+      )}
+    </>
   );
 }
