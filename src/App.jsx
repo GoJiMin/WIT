e3a8a872761ba699916db17af47baf251375b73a
GoJiMin <gojimin3095@gmail.com>
2023-11-10 03:21:07 +0900

    fix: Mixed content 에러 해결

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import styles from "./App.module.css";
 import { QueryClient } from "@tanstack/react-query";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { AuthContextProvier } from "./context/AuthContext";
-import { useRef } from "react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
